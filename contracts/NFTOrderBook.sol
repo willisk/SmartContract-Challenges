ece34446af6b0370a37d985258bf670b09ff1d02
willisk <kurtwillis@live.com>
2022-03-15 22:10:35 +0100

    non-reentrant

--- contracts/NFTOrderBook.sol
+++ contracts/NFTOrderBook.sol
@@ -1,9 +1,10 @@
-//SPDX-License-Identifier: Unlicense
+//SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
 import '@openzeppelin/contracts/token/ERC721/IERC721.sol';
+import '@openzeppelin/contracts/security/ReentrancyGuard.sol';
 
-contract NFTOrderBook {
+contract NFTOrderBook is ReentrancyGuard {
     struct Offer {
         bytes32 hash;
         address bidder;
@@ -20,13 +21,11 @@ contract NFTOrderBook {
 
     /* ------------- Exchange ------------ */
 
-    // need to place offers in eth upfront
-    // refunds not available yet
     function placeOffers(
         IERC721[] calldata collections,
         uint256[] calldata tokenIds,
         uint256[] calldata prices
-    ) public payable {
+    ) public payable nonReentrant {
         uint256 totalValue;
 
         for (uint256 i; i < collections.length; i++) {
@@ -36,12 +35,22 @@ contract NFTOrderBook {
             totalValue += prices[i];
         }
 
-        require(msg.value >= totalValue, "Ser, where's the money?");
+        require(msg.value >= totalValue, 'Ser, the money?');
     }
 
-    function acceptOffers(bytes32[] memory hashes) public {
+    function acceptOffers(bytes32[] memory hashes) public nonReentrant {
         Offer[] memory offers = getOffers(hashes);
-        _acceptOffers(offers);
+        for (uint256 i; i < offers.length; i++) {
+            Offer memory offer = offers[i];
+
+            require(msg.sender != offer.bidder, 'No tricks, ser.');
+            require(offer.collection.ownerOf(offer.tokenId) == msg.sender, 'Ser, the nft please?');
+
+            delete _offers[offer.hash];
+
+            offer.collection.transferFrom(msg.sender, offer.bidder, offer.tokenId);
+            payable(msg.sender).transfer((offer.price * 8) / 10); // 20% tax
+        }
     }
 
     /* ------------- View ------------ */
@@ -57,21 +66,5 @@ contract NFTOrderBook {
     function getOffers(bytes32[] memory hashes) public view returns (Offer[] memory offers) {
         offers = new Offer[](hashes.length);
         for (uint256 i; i < hashes.length; i++) offers[i] = _offers[hashes[i]];
-    }
-
-    /* ------------- Private ------------ */
-
-    function _acceptOffers(Offer[] memory offers) private {
-        for (uint256 i; i < offers.length; i++) {
-            Offer memory offer = offers[i];
-
-            require(msg.sender != offer.bidder, 'No tricks, ser.');
-            require(offer.collection.ownerOf(offer.tokenId) == msg.sender, 'Ser, the nft please?');
-
-            offer.collection.transferFrom(msg.sender, offer.bidder, offer.tokenId);
-            payable(msg.sender).transfer((offer.price * 8) / 10); // 20% tax
-
-            delete _offers[offer.hash];
-        }
     }
 }
